0c7819f91edb13d59aac10972d8857a5cc8ad95e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-13 15:51:17 -0300

    faces

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 (function(){
+	var images = [];
+
+	for(var i = 0; i < 16; i++){
+		var img = {
+			src: "img/"+ i +".jpg",
+			id: i%8	
+		};
+		images.push(img);
+	}
+
+
 	//chama a função de inicialização do jogo
 	startGame();
 	
 	//função de inicialização do jogo
 	function startGame(){
+		var frontFaces = document.getElementsByClassName("front");
+
 		//posicionamento das cartas
 		for(var i = 0; i < 16; i++){
 			var card = document.querySelector("#card" + i);
@@ -11,6 +24,10 @@
 			card.style.top = i < 8 ? 5 + "px" : 250 + "px";
 
             card.addEventListener("click", flipCard, false);
+
+			frontFaces[i].style.background = "url('"+ images[i].src +"')";
+			frontFaces[i].setAttribute("id",images[i].id);
+			console.log(frontFaces[i].id);
 		}
 	}
 
