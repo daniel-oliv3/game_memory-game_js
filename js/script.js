5d03e6b6a971d4f0cf5404605356a445a5de20d4
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-12-13 12:30:19 -0300

    Rotacionando as Cartas

--- js/script.js
+++ js/script.js
@@ -9,6 +9,16 @@
 			var card = document.querySelector("#card" + i);
 			card.style.left = (i % 8 === 0) ? 5 + "px" : (i % 8) * 165 + 5 + "px";
 			card.style.top = i < 8 ? 5 + "px" : 250 + "px";
+
+            card.addEventListener("click", flipCard, false);
 		}
 	}
+
+    //Rotacionando as Cartas
+    function flipCard(){
+        var faces = this.getElementsByClassName("face");
+        faces[0].classList.toggle("flipped");
+        faces[1].classList.toggle("flipped");
+        
+    }
 }());
